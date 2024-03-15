6bcb23795ac43140626e37be44f06631ea9717d4
Nick Jones <nico.rjones@outlook.com>
2024-03-15 02:55:56 -0700

    exercise 2.1 - 2.7

--- part2/course-information/src/App.jsx
+++ part2/course-information/src/App.jsx
@@ -18,6 +18,26 @@ const App = () => {
       name: 'State of a component',
       exercises: 14,
       id: 3
+    },
+    {    
+      name: 'Arbitray Number',
+      exercises: 4,
+      id: 4
+    },
+    {
+      name: 'Of Courses',
+      exercises: 5,
+      id: 5
+    },
+    {
+      name: 'I could keep going',
+      exercises: 25,
+      id: 6
+    },
+    {
+      name: 'And it will work',
+      exercises: 2,
+      id: 7
     }
   ]
 }
