f5843f81e6e34a8c4f1b50460ed9a198a33d37e4
Nick Jones <nico.rjones@outlook.com>
2024-03-20 01:30:17 -0700

    exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
   const id = 0
-  const [persons, setPersons] = useState([{ 
-    name: 'Arto Hellas',
-    id: id,
-    number: '555-555-5555',
-   }])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchQuery, setSearchQuery] = useState('')
+
+useEffect(() => {
+  axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+}, [])
 
   const filterPersons = (persons, searchQuery) => {
     if (!searchQuery) {
