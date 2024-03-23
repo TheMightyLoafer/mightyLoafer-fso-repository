9c98f26f7b10b9271438e49c9849bc5b9efa1ba6
Nick Jones <nico.rjones@outlook.com>
2024-03-23 02:07:14 -0700

    notes example part 2e, custom error msg

--- examples/part2-examples/src/App.jsx
+++ examples/part2-examples/src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('This is a new note...')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService
@@ -16,7 +17,6 @@ const App = () => {
       }, [])
       
     const toggleImportanceOf = (id) => {
-      const url = `http://localhost:3001/notes/${id}`
       const note = notes.find(n => n.id === id)
       const changedNote = { ...note, important: !note.important }
 
@@ -26,9 +26,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
     }
@@ -60,6 +63,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}</button>
